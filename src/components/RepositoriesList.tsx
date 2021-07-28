ec3157286b0724d310ac7ac5480d3e242bcc6f63
Zomer <zomerzxc.19@gmail.com>
2021-07-28 19:22:11 +0800

    Finish displaying results

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
+import { useSelector } from 'react-redux';
 
 interface IRepositoriesListProps {}
 
 const RepositoriesList: React.FC<IRepositoriesListProps> = ({}) => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const state = useSelector(state => state);
   const { data, error, loading } = useTypedSelector(
     state => state.repositories
   );
+
+  console.log(state);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
@@ -27,6 +31,26 @@ const RepositoriesList: React.FC<IRepositoriesListProps> = ({}) => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error &&
+        !loading &&
+        data.map(name => {
+          const packageName = name.replace(
+            'https://www.npmjs.com/package/',
+            ''
+          );
+          const noDash = packageName.replaceAll('-', ' ');
+          const noNum = noDash.replace(/[0-9]/g, '');
+          const finalName = noNum.replaceAll('%', ' ');
+          return (
+            <div key={name}>
+              <a href={name} target='_blank'>
+                {finalName}
+              </a>
+            </div>
+          );
+        })}
     </div>
   );
 };
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -20,7 +20,7 @@ export const searchRepositories = (term: string) => {
       );
 
       const names = data.objects.map((result: any) => {
-        return result.package.name;
+        return result.package.links.npm;
       });
 
       dispatch({
