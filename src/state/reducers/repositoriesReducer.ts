9f82d5239ce332cf5d416d957bb9273da99d8bd3
Zomer <zomerzxc.19@gmail.com>
2021-07-28 17:02:10 +0800

    Add reducer for repositories

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,11 +1,39 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
 interface IRepositoriesState {
   loading: boolean;
   error: string | null;
   data: string[];
 }
 
-const reducer = (state: IRepositoriesState, action: any) => {
-  switch (action.type)
+const reducer = (
+  state: IRepositoriesState,
+  action: Action
+): IRepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
