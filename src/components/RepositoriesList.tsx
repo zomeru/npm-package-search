e6fc32327654e9bfcb68a4d7fc444e27a83a51fb
Zomer <zomerzxc.19@gmail.com>
2021-07-28 18:26:52 +0800

    Bind action creators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useActions } from '../hooks/useActions';
 
 interface IRepositoriesListProps {}
 
 const RepositoriesList: React.FC<IRepositoriesListProps> = ({}) => {
+  const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form action=''>
-        <input type='text' />
+      <form action='' onSubmit={onSubmit}>
+        <input
+          type='text'
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+        />
         <button>Search</button>
       </form>
     </div>
